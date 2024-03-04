d9147d61c6c9f3e86d369d4c2f41734f2d006644
miguelrp99 <miguelrodriguez.p99@gmail.com>
2024-03-04 02:46:33 +0100

    fixes

---
+++ backend/middleware/useLogin.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import { toast } from "sonner";
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const login = async ({ username, password }) => {
+    setLoading(true);
+
+    try {
+      const res = await fetch("http://localhost:8080/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await res.json();
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      localStorage.setItem("authUser", JSON.stringify(data));
+      setAuthUser(data);
+      toast.success("Logged in successfully!");
+    } catch (error) {
+      toast.error("An error occurred. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, login };
+};
+
+export default useLogin;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,8 +9,13 @@ import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express();
+// const app = express(); // como tenemos socket está en socket.js
+import { app, server } from "./socket/socket.js"; //usamos esta app del socket
+
 const PORT = process.env.PORT || 5000;
+
+// Esto nos da el root de la carpeta
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -21,12 +27,20 @@ app.use("/api/auth", authRoutes); //Creamos un middleware que funciona como cont
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); //Para servir el frontend
+
+//Esto hace que el front corra en el mismo puerto que el back al hacer una build
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 app.get("/", (req, res) => {
   res.send("Server is ready!!");
 });
 
 //Cuando la aplicación empiece a escuchar entonces la conectamos a la base de datos
-app.listen(PORT, () => {
+//Ahora en lugar de usar app.listen usamos server.listen porque estamos usando el servidor del socket también
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server started on http://localhost:${PORT}`);
 });
